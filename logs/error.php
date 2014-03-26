7d16a1a9e04a47212ec7af835fb946177a0d1817
matiasmedina5 <matias.medina@modelitsoftware.com>
2014-03-26 12:58:26 -0300

    proyecto joomla

---
+++ logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-03-26 14:22:04 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2014-03-26	14:22:04	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2014-03-26	14:22:50	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2014-03-26	14:24:42	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2014-03-26	14:24:51	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
